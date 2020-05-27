ae5700531ebfde59a3afe5e62c8dc1168a26d56d
Nicolas Mella <nmella@uc.cl>
2020-05-27 19:13:40 -0400

    ya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-//import github from '../img/github-icon.svg'
+import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -63,18 +63,12 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-           //   <Link className="navbar-item" to="/products">
-           //     Products
-            //  </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-            //  <Link className="navbar-item" to="/contact/examples">
-            //    Form Examples
-            //  </Link>
             </div>
           </div>
         </div>
