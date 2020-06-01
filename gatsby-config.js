0c818b7714f21322068d61c80f8db1a1fe96c293
Nicolas Mella <nmella@uc.cl>
2020-06-01 15:18:30 -0400

    title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Level1 - Desarrollo Ecommerce',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Desarrollo Ecommerce. Magento, Vtex, Shuup, Sharetribe, React Commerce, Spree Commerce, Oracle Commerce, Salesforce, SAP Hybris, Intershop, OroCommerce.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
